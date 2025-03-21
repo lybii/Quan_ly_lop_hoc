651825288a0c45e705e73916e612fc860b8b1737
lybii <lethihaiyen2004hn@gmail.com>
2025-03-21 17:47:18 +0700

    Cập nhật thư mục src

---
+++ src/main/java/com/example/quan_ly_lop_hoc/QuanLyLopHocApplication.java
@@ -0,0 +1,13 @@
+package com.example.quan_ly_lop_hoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuanLyLopHocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuanLyLopHocApplication.class, args);
+	}
+
+}
